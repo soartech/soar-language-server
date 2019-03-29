29733edeb0e8c4cbdf99f5a4b2ba2e85d4731fc3
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2019-03-28 22:34:40 -0400

    Flesh out lsp-soar.el package.

--- integrations/emacs/lsp-soar.el
+++ integrations/emacs/lsp-soar.el
@@ -1,9 +1,33 @@
-;; Work in progress.
-;;
-;; This depends on the lsp-mode package, and a pre-existing soar-mode.
+;;; lsp-soar.el --- Language server support for Soar
+
+;; Version 0.1
+;; Author: Anthony Deschamps <anthony.j.deschamps@gmail.com>
+;; Package-Requires: ((lsp-mode "6.0"))
+;; Keywords: lsp, soar
+;; URL: https://github.com/soartech/soar-language-server
+
+
+;;; Commentary:
+
+;; This package provides language server support for the Soar
+;; language. To enable, call (lsp) in soar-mode-hook and
+;; tcl-mode-hook.
+
+;;; Code:
+(require 'lsp)
+
+(defcustom lsp-soar-executable
+  "soar-language-server"
+  "Path to the Soar language server executable."
+  :type 'file
+  :group 'lsp-soar)
 
 (lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection "soar-language-server")
-                  :major-modes '(soar-mode)
-                  :priority -1
-                  :server-id 'soar-ls))
+ (make-lsp-client
+  :new-connection (lsp-stdio-connection (lambda () "soar-language-server"))
+  :major-modes '(soar-mode tcl-mode)
+  :priority -1
+  :server-id 'soar-ls))
+
+(provide 'lsp-soar)
+;;; lsp-soar.el ends here
