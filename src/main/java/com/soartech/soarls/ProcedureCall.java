f2527cd658f53c98cf7c88bed5fd3df2f09ee254
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2019-04-04 10:51:37 -0400

    Require call site when constructing procedure call record.

--- src/main/java/com/soartech/soarls/ProcedureCall.java
+++ src/main/java/com/soartech/soarls/ProcedureCall.java
@@ -6,12 +6,16 @@ import java.util.List;
 /** A record of a procedure being called and its associated
  * metadata. */
 class ProcedureCall {
+    /** The AST node containing the call and its arguments. */
+    final TclAstNode callSite;
+
     /** Where and how the procedure was defined. */
     ProcedureDefinition definition;
 
-    /** The AST node containing the call and its arguments. */
-    TclAstNode callSite;
-
     /** The result that was returned by this call. */
     String result;
+
+    ProcedureCall(TclAstNode callSite) {
+        this.callSite = callSite;
+    }
 }
